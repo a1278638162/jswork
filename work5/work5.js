f617684b3e59fe913471d62621e5ca2e5d38006e
a1278638162 <1278638162@qq.com>
2019-10-15 00:06:23 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,5 +1,14 @@
 //留位置写9X9
-
+str = '<table border="1">'
+for(let i=0;i<9;++i){
+    str += '<tr>'
+    for(var j=0;j<9;++j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table1').innerHTML = str
 
 //倒久久乘法表
 str = '<table border="1">'
@@ -13,13 +22,14 @@ for(let i=9;i>0;--i){
 str +='</table>'
 document.getElementById('table2').innerHTML = str
 //分割线
-str ='<table border="2">'
+
+str = '<table border="1">'
 for(let i=9;i>0;--i){
     str += '<tr>'
-    for(var j=9;j<=i;--j){
-        str += '<td>' + j +'*' + i + '=' + ( j * i ) + '</td>'
+    for(let j=i;j>0;--j){
+        str += '<td>'+ j +'*'+ i +'='+j*i+'</td>'
     }
-    str += '</tr>'
-    }
-    str += '</table>'
-    document.getElementById('table3').innerHTML = str
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table3').innerHTML = str
